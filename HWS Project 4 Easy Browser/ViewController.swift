704c15161e4d1ecb5c2151687607f5841d9c833d
Valentyn Bochkarov <bochkarov90@gmail.com>
2020-08-23 15:48:49 +0200

    Initial Commit

---
+++ HWS Project 4 Easy Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS Project 4 Easy Browser
+//
+//  Created by Bochkarov Valentyn on 23/08/2020.
+//  Copyright © 2020 Bochkarov Valentyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
